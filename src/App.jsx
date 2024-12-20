4a27e7cac789266a1f57390130f142fd9782b874
Elena <ek28150@gmail.com>
2024-12-19 20:29:05 -0800

    Deleted useSemiPersistentState function, defined a new promise to mimic asynchronous data fetching, added Loading state and Loading indicator

--- src/App.jsx
+++ src/App.jsx
@@ -2,40 +2,59 @@ import * as React from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
-const useSemiPersistentState = () => {
+function App () {
 
   // Retrieve the saved todo list from localStorage or default to an empty array
   const savedTodoList = JSON.parse (localStorage.getItem ('savedTodoList')) || [];
 
-  const [todoList, setTodoList] = React.useState(savedTodoList);
+   // Initialize state with the saved data from localStorage
+  const [todoList, setTodoList] = React.useState (savedTodoList); // in the assignment "Update the default state for todoList to be an empty Array" ???
+  const [isLoading, setIsLoading] = React.useState (true);
 
-  // Save the todo list to localStorage whenever it changes
-  React.useEffect(() => {
-    localStorage.setItem('savedTodoList', JSON.stringify(todoList));
-    }, [todoList]);
+   // Simulate fetching data
+  React.useEffect (() => {
+  new Promise ((resolve , reject) => {
+    setTimeout (() => {
+      resolve ({ data: { todoList: [] }}); // Simulate an empty todo list for now
+      }, 2000);
+    })
+    .then ((result) => {
+      setTodoList (result.data.todoList);
+      setIsLoading (false);
+    })
+    // optional not in the assignment of this week
+    .catch((error) => {
+      console.error('Error fetching data:', error); // Handle errors
+      setIsLoading(false); // Make sure to set loading to false even on error
+    });
+  },[]);
 
-  return [todoList, setTodoList]; 
-}
-
-
-function App () {
-
-const [todoList, setTodoList] = useSemiPersistentState();
-
+  // Save the todo list to localStorage whenever it changes and is loading is false
+  React.useEffect (() => {
+    if (!isLoading) {
+      localStorage.setItem ('savedTodoList', JSON.stringify(todoList));
+    }
+  }, [todoList]);
+    
 function AddTodo (newTodo) {
   setTodoList ((prevTodoList) => [...prevTodoList, newTodo])
 }
 
 const removeTodo = (id) => {
-  const newTodoList = todoList.filter(
+  const newTodoList = todoList.filter (
     (todo) => todo.id !== id);
-    setTodoList(newTodoList);
+    setTodoList (newTodoList);
 }
 
 return (
   <>
   <h1> Todo List </h1>
-  <TodoList  todoList = {todoList} onRemoveTodo = {removeTodo} /> {/* Pass removeTodo as prop */}
+  {isLoading ? (
+    <p> Loading... </p>
+  ) : (
+  <TodoList  todoList = {todoList} onRemoveTodo = {removeTodo} /> 
+  )}
+  
   <AddTodoForm onAddTodo = {AddTodo} />
   </>
   );
